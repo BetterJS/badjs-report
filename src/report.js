97416a363fce21b77969dc4eba4a49153473f172
Chris Cai <caihuijigood@gmail.com>
2015-02-10 17:26:55 +0800

    修改上报的错误

--- src/report.js
+++ src/report.js
@@ -124,7 +124,7 @@ var BJ_REPORT = (function(global) {
                 _config.report = (_config.url || "http://badjs2.qq.com/badjs")
                     + "?id=" + id
                     + "&uin=" + parseInt(_config.uin || (document.cookie.match(/uin=\D+(\d+)/) || [])[1], 10)
-                    + "&target=" + encodeURIComponent(location.href)
+                    + "&from=" + encodeURIComponent(location.href)
                     + "&";
                 //!_isInited && _run();
                 _isInited = true;
