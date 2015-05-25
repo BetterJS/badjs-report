751c29c9e698974f72fd53efb573807f5a7785fb
yorts52 <yorts52@qq.com>
2015-05-25 20:30:33 +0800

    rebuild

--- src/bj-report.js
+++ src/bj-report.js
@@ -1,4 +1,4 @@
-﻿/*!
+/*!
  * @module report
  * @author kael
  * @date @DATE
@@ -14,14 +14,13 @@ var BJ_REPORT = (function(global) {
         _error.push({
             msg: msg,
             target: url,
-            rowNum : line,
-            colNum : col,
+            rowNum: line,
+            colNum: col,
             error: error
         });
 
         _send();
-        orgError && orgError.apply(global , arguments);
-
+        orgError && orgError.apply(global, arguments);
     };
 
     var _config = {
@@ -32,7 +31,7 @@ var BJ_REPORT = (function(global) {
         level: 4, // 1-debug 2-info 4-error 8-fail
         ignore: [],
         delay: 100,
-        submit : null
+        submit: null
     };
 
     var _isOBJ = function(o, type) {
@@ -48,8 +47,12 @@ var BJ_REPORT = (function(global) {
             for (var key in error) {
                 var value = error[key] || "";
                 if (value) {
-                    if(typeof value =='object'){
-                        value = JSON.stringify(value);
+                    if (_isOBJ(value)) {
+                        try {
+                            value = JSON.stringify(value);
+                        } catch (err) {
+                            value = "<detect by report> " + err.message;
+                        }
                     }
                     stringify.push(key + ":" + value);
                     param.push(key + "=" + encodeURIComponent(value));
@@ -58,31 +61,32 @@ var BJ_REPORT = (function(global) {
             }
         }
 
-        //  aa[0]=0&aa[1]=1
-        //  aa:0,aa:1
-        //  aa=0&aa=1
+        // msg[0]=msg&target[0]=target -- combo report
+        // msg:msg,target:target -- ignore
+        // msg=msg&target=target -- report with out combo
         return [params.join("&"), stringify.join(","), param.join("&")];
     };
 
-
-
-    var _submit = function (url ){
-        if(_config.submit) {
+    var _imgs = [];
+    var _submit = function(url) {
+        if (_config.submit) {
             _config.submit(url);
-        }else {
-            var img = new Image();
-            img.src = url;
+        } else {
+            var _img = new Image();
+            _imgs.push(_img);
+            _img.src = url;
         }
     };
 
-    var error_list = [], comboTimeout = false, comboTimeoutId;
+    var error_list = [];
+    var comboTimeout = 0;
     var _send = function() {
         if (!_config.report) return;
 
         while (_error.length) {
             var isIgnore = false;
             var error = _error.shift();
-            var error_str = _error_tostring(error, error_list.length); // JSON.stringify(error);
+            var error_str = _error_tostring(error, error_list.length);
             for (var i = 0, l = _config.ignore.length; i < l; i++) {
                 var rule = _config.ignore[i];
                 if ((_isOBJ(rule, "RegExp") && rule.test(error_str[1])) ||
@@ -97,39 +101,29 @@ var BJ_REPORT = (function(global) {
                 } else {
                     _submit(_config.report + error_str[2] + "&_t=" + (+new Date));
                 }
-
-                _config.onReport && (_config.onReport(_config.id , error ));
+                _config.onReport && (_config.onReport(_config.id, error));
             }
         }
 
-        if (_config.combo) {
-            if(comboTimeout){
-                return ;
-            }
-            comboTimeout = true;
-
-            comboTimeoutId = setTimeout(function () {
-                var count = error_list.length;
-                 _submit(_config.report + error_list.join("&") + "&count=" + count + "&_t=" + (+new Date));
-                 error_list = [];
-                 comboTimeout = false;
-             }, _config.delay);
+        if (_config.combo && !comboTimeout) {
+            comboTimeout = setTimeout(function() {
+                _submit(_config.report + error_list.join("&") + "&count=" + error_list.length + "&_t=" + (+new Date));
+                comboTimeout = 0;
+                error_list = [];
+            }, _config.delay);
         }
-
     };
 
-
-    var _isInited = false;
-    var report =  {
+    var report = {
         push: function(msg) { // 将错误推到缓存池
             _error.push(_isOBJ(msg) ? msg : {
                 msg: msg
             });
+            _send();
             return report;
         },
         report: function(msg) { // 立即上报
             msg && report.push(msg);
-            _send();
             return report;
         },
         init: function(config) { // 初始化
@@ -141,27 +135,14 @@ var BJ_REPORT = (function(global) {
             // 没有设置id将不上报
             var id = parseInt(_config.id, 10);
             if (id) {
-                _config.report = (_config.url || "http://badjs2.qq.com/badjs")
-                    + "?id=" + id
-                    + "&uin=" + parseInt(_config.uin || (document.cookie.match(/\buin=\D+(\d+)/) || [])[1], 10)
-                    + "&from=" + encodeURIComponent(location.href)
-                    + "&";
-                //!_isInited && _run();
-                _isInited = true;
+                _config.report = (_config.url || "http://badjs2.qq.com/badjs") + "?id=" + id + "&uin=" + parseInt(_config.uin || (document.cookie.match(/\buin=\D+(\d+)/) || [])[1], 10) + "&from=" + encodeURIComponent(location.href) + "&";
             }
-            _error = [];
-            error_list = [];
-            clearTimeout(comboTimeoutId);
             return report;
         }
     };
 
-
     return report;
-
 }(window));
-
-
 
 if (typeof exports !== 'undefined') {
     if (typeof module !== 'undefined' && module.exports) {
